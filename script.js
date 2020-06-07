b8126fcf247070c4ffb82ea58a476a8adb11c610
Chuffrey4 <Chuffrey4@users.noreply.github.com>
2020-06-06 18:04:41 -0700

    i think i finished it, could also have some giant raging bugs in it tho

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function Minesweeper() {
     this.canvasWidth = 600, this.canvasHeight = 600;
     this.gfx = this.canvas.getContext("2d");
     this.fieldWidth = 25, this.fieldHeight = 25; // match this w/h ratio to canvas' for square mines.
-    this.generatedMines = 1;
+    this.generatedMines = 75;
     this.minesFlagged = 0;
 
     this.gameOver = false;
@@ -150,7 +150,7 @@ function Minesweeper() {
     if (!this.searched[idx] && !this.flagged[idx]) {
       if (this.field[idx] <= 0) {
         this.searched[idx] = true;
-        this.gfx.fillStyle = "#FFFFFF";
+        this.gfx.fillStyle = (x + (y % 2)) % 2 == 0 ? "#FFFFFF" : "#EEEEEE";
         this.gfx.fillRect(x * this.mineWidth, y * this.mineHeight, this.mineWidth, this.mineHeight);
         var xp = x < this.fieldWidth - 1, xn = x > 0; // x +/- available
         var yp = y < this.fieldHeight - 1, yn = y > 0; // y +/- available
@@ -164,7 +164,7 @@ function Minesweeper() {
         if (yn) this.searchCell(x, y - 1);
       } else if (this.field[idx] < 9) {
         this.searched[idx] = true;
-        this.gfx.fillStyle = "#FFFFFF";
+        this.gfx.fillStyle = (x + (y % 2)) % 2 == 0 ? "#FFFFFF" : "#EEEEEE";
         this.gfx.fillRect(x * this.mineWidth, y * this.mineHeight, this.mineWidth, this.mineHeight);
         this.gfx.fillStyle = "#000000";
         this.gfx.fillText(this.field[idx], (x + 0.5) * this.mineWidth, (y + 0.5) * this.mineHeight);
@@ -195,7 +195,7 @@ function Minesweeper() {
     this.setup();
   }
   this.enableFlagging = function () {
-    this.flagging = false;
+    this.flagging = true;
     document.body.style.backgroundColor = "darkgreen";
   }
   this.disableFlagging = function () {
@@ -203,8 +203,179 @@ function Minesweeper() {
     document.body.style.backgroundColor = "darkred";
   }
 }
+
+function Sweeperbot(ms) {
+  this.ms = ms; // minesweeper object to be botted.
+
+  this.setup = function() {
+    this.restart();
+  }
+
+  this.restart = function () {
+    this.ms.restart();
+    var totalSize = this.ms.fieldWidth*this.ms.fieldHeight;
+    this.solved = [];
+    for (var i=0;i<totalSize;i++) {
+      this.solved[i] = false;
+    }
+    this.useful = []; // list of indices in the minefield that are useful to solve using bot.
+    this.startedSearching = false;
+  }
+
+  this.tick = function () {
+    if (this.ms.gameOver) { // restarting counts as a tick
+      this.restart();
+    } else {
+      var moveCompleted = false;
+      if (this.startedSearching) {
+        var ogTotal = this.useful.length;
+        var cellsSearched = 0;
+        while(!moveCompleted) {
+          if (cellsSearched<ogTotal) {
+            var cellToTry = this.useful.shift();
+            cellsSearched++;
+            if (this.solveCell(cellToTry)) {
+              moveCompleted = true;
+              this.solved[cellToTry] = true;
+            } else {
+              this.useful.push(cellToTry);
+            }
+          } else {
+            this.searchRandom();
+            this.findAllUseful();
+            moveCompleted = true;
+          }
+        }
+      } else {
+        this.searchRandom();
+        this.findAllUseful();
+        this.startedSearching = true;
+      }
+    }
+  }
+
+  this.searchRandom = function () {
+    this.ms.disableFlagging();
+    this.ms.triggerCell(Math.floor(Math.random()*this.ms.fieldWidth),
+      Math.floor(Math.random()*this.ms.fieldHeight));
+  }
+
+  this.findAllUseful = function () {
+    this.useful = [];
+    var totalSize = this.ms.fieldWidth * this.ms.fieldHeight;
+    for (var i=0;i<totalSize;i++) {
+      if (this.ms.searched[i]&&this.ms.field[i]>0&&!this.solved[i]) // if the bot has actually searched the field for this value.
+        this.useful.push(i);
+    }
+  }
+
+  this.solveCell = function (idx) {
+    var x = this.ms.getFieldX(idx), y = this.ms.getFieldY(idx);
+    var cellValue = this.ms.field[idx];
+    console.log(`${cellValue} @ ${x}, ${y} :: f=${this.getFlaggedAdjacents(idx)}  u=${this.getUnsearchedAdjacents(idx)}`);
+    if (this.getFlaggedAdjacents(idx) == cellValue) {
+      this.searchAllAdjacents(idx);
+      return true;
+    }
+    if (this.getUnsearchedAdjacents(idx) == cellValue) {
+      this.flagAllAdjacents(idx);
+      return true;
+    }
+    return false;
+  }
+
+  this.adjacentsBool = function (idx) { // starting top-left, clockwise, true false if there is an available cell to bounds.
+    var results = [], x = this.ms.getFieldX(idx), y = this.ms.getFieldY(idx);
+    var xp = x < this.ms.fieldWidth - 1, xn = x > 0; // x +/- available
+    var yp = y < this.ms.fieldHeight - 1, yn = y > 0; // y +/- available
+    results.push(xn && yn);
+    results.push(yn);
+    results.push(xp && yn);
+    results.push(xp);
+    results.push(xp && yp);
+    results.push(yp);
+    results.push(xn && yp);
+    results.push(xn);
+    return results;
+  }
+  this.adjacentsNumber = function (idx) { // starting top-left, clockwise, true false if there is an available cell to bounds.
+    var results = this.adjacentsBool(idx);
+    var adjs = 0;
+    for (var i=0;i<8;i++) {
+      if (results[i]) adjs++;
+    }
+    return adjs;
+  }
+  this.getAdjacentIDX = function (idx,rl) {// idx and relative location 0-7
+    if (rl <= 0 || rl >= 6) idx--; // x-1
+    if (rl <= 4 && rl >= 2) idx++; // x + 1
+    if (rl <= 2) idx -= this.ms.fieldWidth; // y-1
+    if (rl <= 6 && rl >= 4) idx += this.ms.fieldWidth // y + 1
+    if (idx < 0 || idx >= this.ms.fieldWidth * this.ms.fieldHeight) console.error(`getAdjacentIDX out of range: ${idx}, ${rl}`);
+    return idx;
+  }
+  this.getUnsearchedAdjacents = function (idx) {
+    var abs = this.adjacentsBool(idx);
+    var us = 0; // unsearched
+    if (abs[0]&&!this.ms.searched[idx-this.ms.fieldWidth-1]) us++;
+    if (abs[1]&&!this.ms.searched[idx-this.ms.fieldWidth]) us++;
+    if (abs[2]&&!this.ms.searched[idx-this.ms.fieldWidth+1]) us++;
+    if (abs[3]&&!this.ms.searched[idx+1]) us++;
+    if (abs[4]&&!this.ms.searched[idx+this.ms.fieldWidth+1]) us++;
+    if (abs[5]&&!this.ms.searched[idx+this.ms.fieldWidth]) us++;
+    if (abs[6]&&!this.ms.searched[idx+this.ms.fieldWidth-1]) us++;
+    if (abs[7]&&!this.ms.searched[idx-1]) us++;
+    return us;
+  }
+  this.getFlaggedAdjacents = function (idx) {
+    var abs = this.adjacentsBool(idx);
+    var flj = 0; // flagged adjacents
+    if (abs[0]&&this.ms.flagged[idx-this.ms.fieldWidth-1]) flj++;
+    if (abs[1]&&this.ms.flagged[idx-this.ms.fieldWidth]) flj++;
+    if (abs[2]&&this.ms.flagged[idx-this.ms.fieldWidth+1]) flj++;
+    if (abs[3]&&this.ms.flagged[idx+1]) flj++;
+    if (abs[4]&&this.ms.flagged[idx+this.ms.fieldWidth+1]) flj++;
+    if (abs[5]&&this.ms.flagged[idx+this.ms.fieldWidth]) flj++;
+    if (abs[6]&&this.ms.flagged[idx+this.ms.fieldWidth-1]) flj++;
+    if (abs[7]&&this.ms.flagged[idx-1]) flj++;
+    return flj;
+  }
+  this.flagAllAdjacents = function(idx) {
+    var abs = this.adjacentsBool(idx);
+    var x = this.ms.getFieldX(idx), y = this.ms.getFieldY(idx);
+    console.log(`flagging all adj at ${x}, ${y}`);
+    this.ms.enableFlagging();
+    if (abs[0]&&!this.ms.flagged[idx-this.ms.fieldWidth-1]) this.ms.triggerCell(x-1,y-1);
+    if (abs[1]&&!this.ms.flagged[idx-this.ms.fieldWidth]) this.ms.triggerCell(x,y-1);
+    if (abs[2]&&!this.ms.flagged[idx-this.ms.fieldWidth+1]) this.ms.triggerCell(x+1,y-1);
+    if (abs[3]&&!this.ms.flagged[idx+1]) this.ms.triggerCell(x+1,y);
+    if (abs[4]&&!this.ms.flagged[idx+this.ms.fieldWidth-1]) this.ms.triggerCell(x+1,y+1);
+    if (abs[5]&&!this.ms.flagged[idx+this.ms.fieldWidth]) this.ms.triggerCell(x,y+1);
+    if (abs[6]&&!this.ms.flagged[idx+this.ms.fieldWidth-1]) this.ms.triggerCell(x-1,y+1);
+    if (abs[7]&&!this.ms.flagged[idx-1]) this.ms.triggerCell(x-1,y);
+  }
+  this.searchAllAdjacents = function (idx) {
+    var abs = this.adjacentsBool(idx);
+    var x = this.ms.getFieldX(idx), y = this.ms.getFieldY(idx);
+    this.ms.disableFlagging();
+    if (abs[0]) this.ms.triggerCell(x-1,y-1);
+    if (abs[1]) this.ms.triggerCell(x,y-1);
+    if (abs[2]) this.ms.triggerCell(x+1,y-1);
+    if (abs[3]) this.ms.triggerCell(x+1,y);
+    if (abs[4]) this.ms.triggerCell(x+1,y+1);
+    if (abs[5]) this.ms.triggerCell(x,y+1);
+    if (abs[6]) this.ms.triggerCell(x-1,y+1);
+    if (abs[7]) this.ms.triggerCell(x-1,y);
+  }
+}
+
 window.minesweeper = new Minesweeper();
+window.sweeperbot = new Sweeperbot(minesweeper);
 function start() {
-  minesweeper.setup();
+  sweeperbot.setup();
+  setInterval(tickit, 50);
+}
+function tickit() {
+  sweeperbot.tick();
 }
 window.addEventListener("load", start);
